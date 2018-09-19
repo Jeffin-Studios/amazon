371db51175a650a118ebb5210b1bca8193e3dde6
Richard Bao <richardbao419@gmail.com>
2018-09-18 18:27:11 -0700

    fix

--- scrape.py
+++ scrape.py
@@ -8,6 +8,8 @@ from urllib.parse import urlparse
 from requests_html import HTMLSession
 
 session = HTMLSession()
+headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
+
 
 def parse(s, first, last):
 	try:
@@ -29,11 +31,9 @@ def getInfo(url):
 	# should store these urls in a csv for future use
 
 def retrieve(ASIN):
-	headers = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.90 Safari/537.36'}
 	url="https://www.amazon.com/dp/"+ASIN 
 	response = session.get(url, headers=headers, verify=False) 
-	print(response)
-	# response.html.search('a-color-price">${}<')[0]) #Extracting the price
+	print(response.html.search('a-color-price">${}<')[0]) #Extracting the price
 
 if __name__ == '__main__':
 	(product_name, ASIN) = getInfo("https://www.amazon.com/MAGTIMES-Rainbow-Anti-Anxiety-Spinner-Relieving/dp/B072LNQBZT/ref=sr_1_5?s=toys-and-games&ie=UTF8&qid=1537316629&sr=1-5&keywords=fidget%20spinner")
